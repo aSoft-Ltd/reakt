9d01a15ae1a2e65e3b01d8c196c43e1c3e9acc2b
andylamax <andylamax@programmer.net>
2020-12-04 06:46:40 +0300

    Bumped version to 0.0.7

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -4,7 +4,7 @@ object vers {
     val nexus_staging = "0.22.0"
 
     object asoft {
-        val reakt = "0.0.6"
+        val reakt = "0.0.7"
         val kotlinx_extensions = "0.0.1"
         val theme = "0.0.4"
         val builders = "1.1.1"
