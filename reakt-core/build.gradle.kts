8a1eca1f230d3d2e07ecf1cb065ba98b0c65de67
andylamax <andylamax@programmer.net>
2020-12-04 05:59:59 +0300

    Allowed nullable generics on react-tables

--- reakt-core/build.gradle.kts
+++ reakt-core/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 dependencies {
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:${versions.kotlinx.coroutines}")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:${vers.kotlinx.coroutines}")
     api("tz.co.asoft:kotlinx-extensions:${vers.asoft.kotlinx_extensions}")
     api("org.jetbrains:kotlin-react:${vers.wrappers.react}")
     api("org.jetbrains:kotlin-styled:${vers.wrappers.styled}")
